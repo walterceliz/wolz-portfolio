afbbedfd51f200d6cfe9217f7b577fc286ec589f
Walter Celiz <walterceliz93@gmail.com>
2020-03-23 18:37:07 -0300

    Componente Navbar, su css y react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Navbar from './Components/Navbar'
+import Home from './Views/Home/Home'
+/* import TodoSkills from './Views/Skills/TodoSkills'
+import Portfolio from './Views/Portfolio/Portfolio'
+import Contact from './Views/Contact/Contact' */
+import './App.css'
+
 
 function App() {
   return (
     <div className="App">
-      
+
+      <BrowserRouter>
+      <Navbar/>
+      <Switch>
+        <Route path='/'  exact component={Home} />    
+        <Route path='/home'  exact component={Home} />
+        {/* <Route path='/skills' component={TodoSkills} />
+        <Route path='/portfolio' component={Portfolio} />
+        <Route path='/contact'  exact component={Contact} /> */}
+      </Switch>
+      </BrowserRouter>
+
     </div>
   )
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from "react" 
+import { Link } from 'react-router-dom'
+import './Navbar.css' 
+
+const Navbar = props => {
+  return (
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+        <Link className="navbar-brand" to="/"><span className="wea1">/</span>logo</Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link button" to="/home"> Inicio </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link button" to="/skills"> Habilidades </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link button" to="/portfolio"> Portfolio </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link button" to="contact"> Contacto </Link>
+              </li>
+            </ul>
+          </div>
+        </div> 
+      </nav>
+    
+  )
+}
+
+export default Navbar
