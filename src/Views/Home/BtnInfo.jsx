14d98807ee0c39a646cab30d4ed63a84d85e9ba6
Walter Celiz <walterceliz93@gmail.com>
2020-03-24 14:11:42 -0300

    modal terminado, falta foto personal y logo

--- src/Views/Home/BtnInfo.jsx
+++ src/Views/Home/BtnInfo.jsx
@@ -13,17 +13,54 @@ const BtnInfo = (props) => {
                 <div className="modal-dialog modal-dialog-centered" role="document">
                     <div className="modal-content custom2">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalCenterTitle">i'm Wolz</h5>
+                            <h5 className="modal-title" id="exampleModalCenterTitle">Logo</h5>
                                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                         </div>
                         <div className="modal-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ea soluta dignissimos harum eaque possimus. Sunt aliquid, eius voluptates debitis accusamus officia, beatae ad doloremque eligendi unde modi adipisci dolores.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ea soluta dignissimos harum eaque possimus. Sunt aliquid, eius voluptates debitis accusamus officia, beatae ad doloremque eligendi unde modi adipisci dolores.
+                            
+                        <div className="personalData">
+                            <p className="per">-Mi Data-</p>
+                            <p>Nombre: {props.name}.</p>
+                            <p>Nacionalidad: {props.nac}.</p>
+                            <p>Provincia: {props.prov}</p>
+                            <p>Edad: {props.old}.</p>
+                            <p>Estado civil: {props.est}.</p>
+                        </div>
+                        
+
+                        {/* <div className="presentation">
+                            <p>Si me tuviera que presentar diria que soy algo Geek, googlero de alma y tal vez un bicho raro.
+                            Mis pasatiempos son los deportes, salidas con amigos, vicio en PC, consumir multimedia sea series, anime, pelis y ohhh, el maldito pero amado youtube!!.<br/>   
+                            Empece a programar de grande cerca de mis 24 años por mero interes y termine enganchando a este mundo, el hecho de que salga algo nuevo cada dia me llena de intrigas y me hace googlear</p>
+                            <p>Si me tuviera que presentar diria que soy algo Geek, googlero de alma y tal vez un bicho raro.
+                            Mis pasatiempos son los deportes, salidas con amigos, vicio en PC, consumir multimedia sea series, anime, pelis y ohhh, el maldito pero amado youtube!!.<br/>   
+                            Empece a programar de grande cerca de mis 24 años por mero interes y termine enganchando a este mundo, el hecho de que salga algo nuevo cada dia me llena de intrigas y me hace googlear</p>
+                        </div> */}
+                        
+
+                        <div className="education1">
+                            <p className="per">-Educacion-</p>
+                            <p>Primaria: {props.prim}</p>
+                            <p>Secundaria: {props.sec}.</p>
+                            <p>Terciaria: {props.terc}</p>
+                            <p>Ingles: {props.ing}</p>
+                        </div>
+                        
+
+                        <div className="education2">
+                            <p className="per">-Cursos-</p>
+                            <p>{props.rolling}</p>
+                            <p className="per">Udemy:</p>   
+                            <p>_{props.ud1}</p>
+                            <p>_{props.ud2}</p> 
+                            <p>{props.ud3}</p>
+                        </div>        
+                        
+
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-primary">Descargar</button>
                             <button type="button" className="btn btn-secondary" data-dismiss="modal">Cerrar</button>
                         </div>
                     </div>
--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -6,11 +6,26 @@ import BtnInfo from './BtnInfo'
 const Home = () => {
     return (
         <div className="homeContainer">
-            <BtnInfo/>
             <Info
                 name="Walter"
                 compname="Wolz"
                 iam="Desarrollador Full Stack JavaScript"
+
+            />
+            <BtnInfo
+                name="Walter, Gabriel Celiz"
+                nac="Argentina"
+                prov="Tucuman"
+                old="27 años"
+                est="Soltero"
+                prim="Colegio Inmaculada Concepcion B.R.S."
+                sec="Colegio Inmaculada Concepcion B.R.S."
+                terc="Tecnicatura en Desarrollo de Software, Instituto 9 de julio, Tucuman (cursado completo, materias pendientes)"
+                ing="Academia Rush, Tucuman (actuamente b1)"
+                rolling="Full stack JavaScript en RollingCode School"
+                ud1="Fundamentos de programacion"
+                ud2="Fundamentos de Diseño Web (html, css, javascript)"
+                ud3="React (fundamentos, core, ciclos de vida, hooks, utildiades, router, redux)"
             />
         </div>
     )
