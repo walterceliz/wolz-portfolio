b3a121690f028b4ab13df4313b20020edc63b52b
Walter Celiz <walterceliz93@gmail.com>
2020-03-24 15:55:17 -0300

    proximamente wachin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import './App.css'
 
 import Navbar from './Components/Navbar'
 import Home from './Views/Home/Home'
-/* import TodoSkills from './Views/Skills/TodoSkills'
+//import TodoSkills from './Views/Skills/TodoSkills'
 import Portfolio from './Views/Portfolio/Portfolio'
-import Contact from './Views/Contact/Contact' */
-import './App.css'
+//import Contact from './Views/Contact/Contact'
+
 
 
 function App() {
@@ -18,9 +19,9 @@ function App() {
       <Switch>
         <Route path='/'  exact component={Home} />    
         <Route path='/home'  exact component={Home} />
-        {/* <Route path='/skills' component={TodoSkills} />
+        {/* <Route path='/skills' component={TodoSkills} /> */}
         <Route path='/portfolio' component={Portfolio} />
-        <Route path='/contact'  exact component={Contact} /> */}
+        {/* <Route path='/contact'  exact component={Contact} /> */}
       </Switch>
       </BrowserRouter>
 
---
+++ src/Views/Portfolio/Portfolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Portfolio.css'
+
+const Portfolio = (props) => {
+    return(
+        <div className="portBack">
+            <h2>Pagina en Construccion</h2>
+        </div>
+    )
+}
+
+export default Portfolio
